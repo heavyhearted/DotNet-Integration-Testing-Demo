02d3a44fb6419b465cbe92d6679fda1a7d5b2289
Dessy Dimitrova <desislava.d@icloud.com>
2024-10-10 18:30:25 +0300

    Revert "Implement Snowboard update - PUT"

--- SnowboardShop.Api/ApiEndpoints.cs
+++ SnowboardShop.Api/ApiEndpoints.cs
@@ -14,10 +14,9 @@ public static class ApiEndpoints
 
         public const string GetAll = Base;
         
-        public const string Update = $"{Base}/{{id:guid}}";
-        
         // public const string Get = $"{Base}/{{idOrSlug}}";
-        
+        // 
+        // public const string Update = $"{Base}/{{id:guid}}";
         // public const string Delete = $"{Base}/{{id:guid}}";
     }
 }
