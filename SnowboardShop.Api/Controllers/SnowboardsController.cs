bd8fcb4200bd5b1ddad3fbe26ea5b7a9ebcf2995
Dessy Dimitrova <desislava.d@icloud.com>
2024-10-14 18:14:06 +0300

    Add authorization policies

--- SnowboardShop.Api/Controllers/SnowboardsController.cs
+++ SnowboardShop.Api/Controllers/SnowboardsController.cs
@@ -1,13 +1,11 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SnowboardShop.Api.Mapping;
-using SnowboardShop.Application.Repositories;
 using SnowboardShop.Application.Services;
 using SnowboardShop.Contracts.Requests;
 
 namespace SnowboardShop.Api.Controllers;
 
-[Authorize]
 [ApiController]
 public class SnowboardsController : ControllerBase
 {
@@ -18,7 +16,7 @@ public class SnowboardsController : ControllerBase
         _snowboardService = snowboardService;
     }
 
-
+    [Authorize(AuthConstants.TrustedMemberPolicyName)]
     [HttpPost(ApiEndpoints.Snowboards.Create)]
     public async Task<IActionResult> Create([FromBody] CreateSnowboardRequest request, CancellationToken token)
     {
@@ -57,6 +55,7 @@ public class SnowboardsController : ControllerBase
         return Ok(snowboardsResponse);
     }
     
+    [Authorize(AuthConstants.TrustedMemberPolicyName)]
     [HttpPut(ApiEndpoints.Snowboards.Update)]
     public async Task<IActionResult> Update([FromRoute] Guid id, [FromBody] UpdateSnowboardRequest request, CancellationToken token)
     {
@@ -71,6 +70,7 @@ public class SnowboardsController : ControllerBase
         return Ok(response);
     }
     
+    [Authorize(AuthConstants.AdminUserPolicyName)]
     [HttpDelete(ApiEndpoints.Snowboards.Delete)]
     public async Task<IActionResult> Delete([FromRoute] Guid id, CancellationToken token)
     {
--- SnowboardShop.Api/Program.cs
+++ SnowboardShop.Api/Program.cs
@@ -1,6 +1,7 @@
 using System.Text;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
+using SnowboardShop.Api;
 using SnowboardShop.Api.Mapping;
 using SnowboardShop.Application;
 using SnowboardShop.Application.Database;
@@ -31,7 +32,16 @@ builder.Services.AddAuthentication(x =>
     };
 });
 
-builder.Services.AddAuthorization();
+builder.Services.AddAuthorization(x =>
+{
+    x.AddPolicy(AuthConstants.AdminUserPolicyName, 
+        p => p.RequireClaim(AuthConstants.AdminUserClaimName, "true"));
+    
+    x.AddPolicy(AuthConstants.TrustedMemberPolicyName,
+        p => p.RequireAssertion(c => 
+            c.User.HasClaim(m => m is { Type: AuthConstants.AdminUserClaimName, Value: "true" }) || 
+            c.User.HasClaim(m => m is { Type: AuthConstants.TrustedMemberClaimName, Value: "true" })));
+});
 
 builder.Services.AddControllers();
 
